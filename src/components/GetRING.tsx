e445edec0ba9dfd6410746f758e4cf73217b1183
fewensa <37804932+fewensa@users.noreply.github.com>
2025-03-05 10:23:36 +0800

    Test

--- src/components/GetRING.tsx
+++ src/components/GetRING.tsx
@@ -1,3 +1,44 @@
+import { CowSwapWidget, CowSwapWidgetParams, TradeType } from '@cowprotocol/widget-react'
+
+
+
+    //  Fill this form https://cowprotocol.typeform.com/to/rONXaxHV once you pick your "appCode"
+    const cowWidgetarams: CowSwapWidgetParams = {
+      "appCode": "My Cool App", // Name of your app (max 50 characters)
+      "width": "100%", // Width in pixels (or 100% to use all available space)
+      "height": "640px",
+      "chainId": 1, // 1 (Mainnet), 100 (Gnosis), 11155111 (Sepolia)
+      "tokenLists": [ // All default enabled token lists. Also see https://tokenlists.org
+          "https://raw.githubusercontent.com/helixbox/silicon/refs/heads/main/definition/tokens/cow-widget.json",
+          "https://files.cow.fi/tokens/CowSwap.json",
+          "https://files.cow.fi/tokens/CoinGecko.json"
+      ],
+      "tradeType": TradeType.SWAP, // TradeType.SWAP, TradeType.LIMIT or TradeType.ADVANCED
+      "sell": { // Sell token. Optionally add amount for sell orders
+          "asset": "USDC",
+          "amount": "100"
+      },
+      "buy": { // Buy token. Optionally add amount for buy orders
+          "asset": "RING",
+          "amount": "0"
+      },
+      "enabledTradeTypes": [ // TradeType.SWAP, TradeType.LIMIT and/or TradeType.ADVANCED
+          TradeType.SWAP,
+          TradeType.LIMIT,
+          TradeType.ADVANCED,
+          TradeType.YIELD
+      ],
+      "theme": "dark", // light/dark or provide your own color palette
+      "standaloneMode": false,
+      "disableToastMessages": false,
+      "disableProgressBar": false,
+      "hideBridgeInfo": false,
+      "hideOrdersTable": false,
+      "images": {},
+      "sounds": {},
+      "customTokens": []
+  }
+
 export default function GetRING() {
     return (
       <>
@@ -10,38 +51,7 @@ export default function GetRING() {
         </div>
         <div className="sm:flex gap-[60px] border-b-solid border-b-[1px] border-b-black p-[60px]">
           <div className="flex flex-coljustify-center">
-            <h2 className="text-[30px] leading-[38px] font-[600]">
-              Decentralized Governance
-            </h2>
-            <p className="text-[16px] leading-[26px]">
-            Transparency and decentralization are fundamental to RingDAO's success. 
-            All decisions and actions are governed by RING token holders.
-            To ensure open governance, we have integrated GitHub for forum discussions and 
-            OpenZeppelin's Governor contract to manage proposals and on-chain voting. 
-            RING token holders can submit proposals and participate in voting, shaping the future of the ecosystem.
-            </p>
-            <div className="flex flex-col sm:flex-row gap-[10px]">
-              <a
-                href="https://github.com/orgs/ringecosystem/discussions"
-                target="_blank"
-                className="cs-buttonHover text-[16px] flex justify-center items-center gap-[10px] w-fit leading-[42px] pl-[15px] pr-[10px] h-[42px] rounded-[21px] border-[1px] border-solid border-black"
-              >
-                Forum Discussion
-                <span className="block w-[24px] h-[24px] rounded-full border-black border-[1px] border-solid leading-[22px] text-center">
-                  {">"}
-                </span>
-              </a>
-              <a
-                href="https://www.tally.xyz/gov/ringdao"
-                target="_blank"
-                className="cs-buttonHover text-[16px] flex justify-center items-center gap-[10px] w-fit leading-[42px] pl-[15px] pr-[10px] h-[42px] rounded-[21px] border-[1px] border-solid border-black"
-              >
-                Proposal or Vote
-                <span className="block w-[24px] h-[24px] rounded-full border-black border-[1px] border-solid leading-[22px] text-center">
-                  {">"}
-                </span>
-              </a>
-            </div>
+            <CowSwapWidget params={cowWidgetarams} />
           </div>
         </div>
       </>
