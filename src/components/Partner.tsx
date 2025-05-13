9a793528a5166a44db7f95da0a47a5e184d022c5
Bear Wang <boundless.forest@outlook.com>
2025-05-13 13:27:04 +0800

    Rebrand DCDAO to RCG (#29)
    * Rebrand DCDAO to RCG
    
    * Update image path
    
    * Update the logo size
    
    * Update the logo size again
    
    * Update the logo size again

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -39,16 +39,16 @@ export default function Partner() {
         <div className="flex-grow flex flex-col flex-shrink-0 sm:w-[50%] p-[100px_20px] sm:p-[60px]">
           <div className="h-[120px] flex items-center mb-[40px]">
             <img
-              src="/images/dcdao.svg"
-              alt="dcdao"
-              className="w-[200px] h-[90px] object-contain object-left"
+              src="/images/rcg.svg"
+              alt="rcg"
+              className="w-[350px] h-[120px] object-contain object-left"
             />
           </div>
           <h3 className="text-[30px] leading-[38px] mb-[10px] font-bold">
-            DCDAO
+            RCG
           </h3>
           <p className="text-[16px] leading-[25px] mb-[20px]">
-            Darwinia Community DAO, DCDAO welcomes anyone striving to innovate in Web3.
+            The RingDAO Community Guild welcomes you to our active community.
           </p>
           <div className="mt-auto flex justify-end">
             <a
